fc929806c78e1b17c6b6722e02b09a87810e8f91
kall!ana <kalliana.glaeser@gmail.com>
2026-07-18 19:08:42 -0300

    hernaça, polimorfismo

--- Main/src/java_curso/Sandero.java
+++ Main/src/java_curso/Sandero.java
@@ -2,6 +2,9 @@ package java_curso;
 
 public class Sandero implements Carro {
 
+    final int limiteDeVelocidade = 150;
+    public int velocidadeAtual = 0;
+
     String modelo;
 
     public Sandero(String modelo) {
@@ -10,6 +13,10 @@ public class Sandero implements Carro {
 
     @Override
     public void acelerar() {
+        if (this.velocidadeAtual < this.limiteDeVelocidade) {
+            this.velocidadeAtual += 10;
+            System.out.println("Acelerando");
+        }
         System.out.println("O carro " + modelo + " está acelerando.");
     }
 
